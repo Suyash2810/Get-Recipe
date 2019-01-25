fceeeaaac4c29c8dcfa47cd0c2e2183e21b59178
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-01-26 02:08:32 +0530

    Validated input errors.

---
+++ Public/Script/UserInterface.js
@@ -0,0 +1,15 @@
+class UserInterface {
+
+    printErrorMessage(message) {
+        // Method for printing the input error.
+        var template = `
+        <div class="alert alert-dismissible" id="alert_box">
+        <a href="#" class="close" data-dismiss="alert" style="color:white;"><i class="fa fa-times"></i></a>
+        <h5 style="color:white;">${message}</h5>
+        </div>
+        `;
+        let container = document.querySelector('#alert_data');
+        container.innerHTML = template;
+
+    }
+}
--- Public/Script/app.js
+++ Public/Script/app.js
@@ -1,5 +1,5 @@
 //Creation of the classes
-
+const ui = new UserInterface();
 
 //Defining all the event listeners
 
@@ -11,7 +11,18 @@ function EventListeners() {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
             let text = document.getElementById('input_data').value;
+            let format = "!~@#$%^&*()_-+>?<{}[]";
+            for (ch of text) {
+                if (format.indexOf(ch) !== -1) {
+                    //Condition when a special character has been found as an input for name.
+                    //In such case an error message would be displayed using a method of an instance of ui.
+                    let message = "Not a valid name. Do not use special characters.";
+                    ui.printErrorMessage(message);
 
+                } else {
+                    console.log("valid");
+                }
+            }
 
         });
     }
