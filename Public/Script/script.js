a513d9985ba18129ffcd6fd3531e971ed1e18f31
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-01-27 13:24:24 +0530

    Made the favicon dynamic.

--- Public/Script/script.js
+++ Public/Script/script.js
@@ -11,5 +11,31 @@ window.onload = function () {
 
     adjust_image_carousel();
 
+    function faviconChanger() {
+        let array = [
+            './images/pexels-photo-434298.jpeg',
+            './images/after-work-drink-alcohol-summer-162571.jpeg',
+            './images/pexels-photo-461427.jpeg',
+            './images/pexels-photo-1282272.jpeg',
+            './images/pexels-photo-1799351.jpeg'
+        ];
 
+        const favicon = document.getElementById('faviconImage');
+        var counter = 0;
+
+
+        setInterval(() => {
+
+            favicon.setAttribute('href', array[counter]);
+
+            counter++;
+            if (counter == array.length - 1) {
+                counter = 0;
+            }
+        }, 1000);
+
+
+    }
+
+    faviconChanger();
 }
