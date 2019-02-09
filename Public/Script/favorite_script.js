4a029ed8013b7d9f36fb7c4e15848afb0483f9e3
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-02-10 01:42:29 +0530

    Retrieved data from the storage.

--- Public/Script/favorite_script.js
+++ Public/Script/favorite_script.js
@@ -9,4 +9,49 @@ window.onload = function () {
     }
 
     setBackgroundMastheadHeight();
+
+    function getDrinksFromStorage() {
+        let drinksList;
+
+        if (localStorage.getItem('drinks') === null) {
+            drinksList = [];
+        } else {
+            drinksList = JSON.parse(localStorage.getItem('drinks'));
+        }
+        return drinksList;
+    }
+
+    function addSelectedDrinksToFavorite(list) {
+        const panel = document.getElementById('panelgroup');
+        var SelectedList = list;
+        var template = `<br>`;
+
+        SelectedList.forEach(
+            (item) => {
+                if (item !== null) {
+                    template += `
+                    <div class="panel panel-default" style="background-color:transparent;">
+                        <div class="panel-header">
+                            <h3 style="font-family: 'Montserrat', sans-serif;font-size:25px;color:white;margin-left:20px;">${item.name}</h3>
+                            
+                        </div>
+                    </div>
+                `;
+                    console.log(template);
+                }
+            }
+        );
+
+        panel.innerHTML = template;
+    }
+
+    function addSelectedDrinksFromStorage() {
+        const list = getDrinksFromStorage();
+        if (list !== null) {
+            addSelectedDrinksToFavorite(list);
+        }
+    }
+
+    addSelectedDrinksFromStorage();
+
 }
