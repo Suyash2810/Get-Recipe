7cac2ed67611343dd9309b1bbd7bced0fe16ab7b
Suyash Awasthi <suyash.awasthi28100182@gmail.com>
2019-02-12 12:53:46 +0530

    Injected template in modal

--- Public/Script/favorite_script.js
+++ Public/Script/favorite_script.js
@@ -31,7 +31,15 @@ window.onload = function () {
                 if (item !== null) {
                     template += `
                     <tr style="color:white;">
-                        <td>${item.id}</td>
+                        <td>
+                        ${item.id}
+                        <br>
+                            <a 
+                            class="btn btn-success viewModalSelected" 
+                            data-id="${item.id}"
+                            data-toggle="modal"
+                            data-target="#myViewModal"
+                            >View</a></td>
                         <td>${item.name}</td>
                         <td><img src="${item.imgDrink}" alt="${item.name}" class="img-fluid" width="80px" height="80px" style="text-align:center;"></td>
                         <td><a class="btn btn-danger" data-id="${item.id}">Remove</a></td>
@@ -53,5 +61,79 @@ window.onload = function () {
     }
 
     addSelectedDrinksFromStorage();
+
+    async function GetSelectedModalById(id) {
+        let url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+        let response = await fetch(url);
+
+        let data = await response.json();
+
+        return {
+            data
+        }
+    }
+
+    function injectdatainModal(IngreMeasureArray) {
+        let temp = ``;
+        IngreMeasureArray.forEach(oneset => {
+            temp += `
+                <h4>${oneset.ingredient} - ${oneset.measure}</h4>
+            `;
+        });
+
+        return temp;
+    }
+
+    function setIngredientsTemplate(data) {
+
+        let ingredientsArray = [];
+        for (let i = 1; i < 16; i++) {
+            const obj = {};
+            if (data[`strIngredient${i}`] !== '') {
+                obj.ingredient = data[`strIngredient${i}`];
+                obj.measure = data[`strMeasure${i}`];
+                ingredientsArray.push(obj);
+            }
+        }
+        return injectdatainModal(ingredientsArray);
+    }
+
+    function viewSelectedModal() {
+        let viewHeader = document.getElementById('viewHeader');
+        let viewBody = document.getElementById('viewBody');
+        let drink;
+        let createdTemplate;
+        document.getElementById('tablegroup').addEventListener('click', (e) => {
+
+            if (e.target.classList.contains('viewModalSelected')) {
+                const id = e.target.getAttribute('data-id');
+                let card = GetSelectedModalById(id);
+
+                card
+                    .then(
+                        (Item) => {
+                            drink = Item.data.drinks[0];
+                            console.log(drink)
+                            viewHeader.innerHTML = `
+                                        <div>
+                                            <img src="${drink.strDrinkThumb}"
+                                            alt="${drink.strDrink}" 
+                                            class="img-fluid" width="100%" height="400px" 
+                                            style="text-align:center;">
+                                        </div>
+                            `;
+                            createdTemplate = setIngredientsTemplate(drink);
+
+                            document.getElementById('ingremeasure').innerHTML = createdTemplate;
+
+                            console.log(viewBody);
+                            console.log(createdTemplate);
+                        }
+                    )
+            }
+        })
+    }
+
+    viewSelectedModal();
 
 }
